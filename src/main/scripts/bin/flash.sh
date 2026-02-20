7c30d0ad7e491cb80030330ee24c802315e8ac64
Daniel Houck <Software@DRHouck.me>
2026-02-19 19:07:47 -0500

    Some flash.sh improvements
    * Add a shebang line; it was not working in bash
    * Add a shellcheck shell= directive and fix shellcheck issues
    * Simplify parameter parsing (and make OpenSBI use actually match
      message)
    * Add warning or error if trying to flash the wrong thing
    * Require response with enter to "Are you sure?" confirmation, not just
      a single keypress, and add `[y/N]` indicator

--- src/main/scripts/bin/flash.sh
+++ src/main/scripts/bin/flash.sh
@@ -1,44 +1,47 @@
-if [ "$1" == "" ]; then
+#!/bin/sh
+
+if [ "$1" = "" ] || [ "$2" = "" ]; then
     printf "Usage:\nflash.sh <path to flash mem> <path to firmware> <OpenSBI (yes/no default: no)>\n"
-    return 1
+    exit 1
 fi
 
-if [ "$2" == "" ]; then
-    printf "Usage:\nflash.sh <path to flash mem> <path to firmware> <OpenSBI (yes/no default: no)>\n"
-    return 1
-fi
-
-if [ "$3" == "" ]; then
+if [ "$3" = "" ] || [ "$3" = "no" ]; then
     echo "Flashing without OpenSBI"
+    usesbi=no
+else
+    echo "Flashing with OpenSBI"
+    usesbi=yes
 fi
 
-if [ "$3" == "no" ]; then
-    echo "Flashing without OpenSBI"
+if [ ! -e "$1" ]; then
+    printf "Error: %s does not exist; are you sure your computer is connected to the flash memory flasher?\n" "$1"
+    exit 1
+elif [ ! -b "$1" ]; then
+    printf "Warning: %s does not appear to be a block device\n" "$1"
 fi
 
-printf "Proceeding will completely erase $1 and flash your custom firmware\nto it. This process is irreversible.\n"
+printf "Proceeding will completely erase %s and flash your custom firmware\nto it. This process is irreversible.\n" "$1"
 
-read -p "Are you sure? " -n 1 -r
-echo
-case "$REPLY" in
+printf "Are you sure? [y/N] "
+read -r reply
+case "$reply" in
     [yY][eE][sS]|[yY])
         printf "Erasing current firmware..."
-        dd if=$2 of=$1 status=none # Work around for empty flash chips
-        dd ibs=$(echo "12*1024*1024" | bc) count=1 if=/dev/zero of=$1 status=none
+        dd if="$2" of="$1" status=none # Work around for empty flash chips
+        dd ibs=$((12*1024*1024)) count=1 if=/dev/zero of="$1" status=none
         printf " Done\n"
-        if [ "$3" = "yes" ]; then
+        if [ "$usesbi" = "yes" ]; then
             printf "Flashing OpenSBI..."
-            dd if=/mnt/builtin/firmware_files/fw_jump.bin of=$1 status=none
+            dd if=/mnt/builtin/firmware_files/fw_jump.bin of="$1" status=none
             printf " Done\n"
             printf "Flashing custom firmware..."
-            dd if=$2 of=$1 seek=2097152 oflag=seek_bytes status=none
+            dd if="$2" of="$1" seek=$((2*1024*1024)) oflag=seek_bytes status=none
             printf " Done\n"
         else
             printf "Flashing custom firmware..."
-            dd if=$2 of=$1 status=none
+            dd if="$2" of="$1" status=none
             printf " Done\n"
         fi
         ;;
     *)
 esac
-
